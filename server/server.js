e2e351aa5adf94507c939567aebf565f640972c7
rachagar20 <rach.agar20@gmail.com>
2024-06-22 15:12:57 +0530

    some new changes

--- server/server.js
+++ server/server.js
@@ -1,27 +1,28 @@
-import express, { response } from "express";
+import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import connectMongoDB from "./database/mongoConnect.js";
 import passport from "passport";
-import passportConfig from "./configuration/passport.js"
+import passportConfig from "./configuration/passport.js";
 import dotenv from "dotenv";
 import router from "./routes/index.js";
+
 dotenv.config();
 
-
-const app=express();
-
+const app = express();
 
 app.use(cors());
-app.use(bodyParser.json())
-//passport
+app.use(bodyParser.json());
+
 app.use(passport.initialize());
 passportConfig(passport);
-
-//routes
-app.use("/",router)
+app.use("/", router);
 
 connectMongoDB();
-app.listen(3000,()=>{
-    console.log("Server is listening on port 3000")
-})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+export default app; 
