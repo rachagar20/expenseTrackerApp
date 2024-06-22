04feec83df0b78c916e5b735afd3df4d29f0e830
rachagar20 <rach.agar20@gmail.com>
2024-06-22 16:20:49 +0530

    new

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,9 @@ import authRouter from "./authRoute.js"
 import userRouter from "./userRoute.js"
 import categoryRouter from "./categoryRoute.js"
 import passport from "passport";
-
+router.use("/",(req,res)=>{
+    res.send("HELLO")
+})
 router.use("/transaction",passport.authenticate("jwt",{session:false}),transactionRouter)
 router.use("/auth",authRouter)
 router.use("/user",userRouter)
